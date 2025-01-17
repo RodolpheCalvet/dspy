1ff4b3ace147eb0e3a04e150c7c648811e3e5d4c
RodolpheCalvet <rodolphecalvet@hotmail.com>
2025-01-17 05:29:14 +0100

    tuto_adele

---
+++ adele/tuto.py
@@ -0,0 +1,12 @@
+
+import dspy
+
+def check():
+    lm = dspy.LM('ollama_chat/llama3.2', api_base='http://localhost:11434', api_key='')
+    dspy.configure(lm=lm)
+
+    print(lm("Say this is a test!", temperature=0.7))  # => ['This is a test!'])
+    lm(messages=[{"role": "user", "content": "Say this is a test!"}])  # => ['This is a test!']
+
+if __name__ == "__main__":
+    check()
